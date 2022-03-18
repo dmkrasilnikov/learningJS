ff7e2922dfe0ac4a65fb6480c4e5c4743fece177
dmKrasilnikov <dmkrasilnikov@yandex.ru>
2022-03-18 19:29:44 +0300

    task9 classes

--- LearningJS/project3/Food/dist/js/scripts.js
+++ LearningJS/project3/Food/dist/js/scripts.js
@@ -81,22 +81,106 @@ window.addEventListener('DOMContentLoaded', ()=>{
 
     //Modal
     const modalBtn = document.querySelectorAll('[data-modal]'),
-          modalCloseBtn = document.querySelectorAll('[data-modal-close]'),
+          modalCloseBtn = document.querySelector('[data-modal-close]'),
           modal = document.querySelector('.modal');
     modalBtn.forEach(function (item){
-       // alert(1);
         item.addEventListener('click', showModal);
     });
-    modalCloseBtn.forEach(function (item){
-        item.addEventListener('click', closeModal);
+    
+    
+    modal.addEventListener('click', (e)=>{
+        console.log(e);
+       if(e.target==modal || e.target==modalCloseBtn) {
+        closeModal();
+       }
+
+    });
+    document.addEventListener('keydown', (e)=>{
+        if (e.code=='Escape' && modal.classList.contains('show')){
+            closeModal();
+        }
     });
     function showModal(){
-        
-        modal.style.display = 'block';
+        modal.classList.add('show');
+        modal.classList.remove('hide');
+        document.body.style.overflow='hidden';
+        clearTimeout(modalTimerId);
+    }
+    function showModalByScroll(){
+        if(document.documentElement.offsetHeight - (document.documentElement.scrollTop + document.documentElement.clientHeight + 1)<0){
+            document.removeEventListener('scroll', showModalByScroll);
+            showModal();
+            
+        }
     }
     function closeModal(){
-     //   alert (2);
-        modal.style.display = 'none';
+        modal.classList.add('hide');
+        modal.classList.remove('show');
+        document.body.style.overflow='';
     }
+
+    const modalTimerId = setTimeout(showModal,5000);
+    document.addEventListener('scroll', showModalByScroll);
+    
+// Menu with classes
+
+class Menu {
+    constructor (title, descr, src, alt, price, parentSelector, ...classes){
+        this.title = title;
+        this.descr = descr;
+        this.src = src;
+        this.alt = alt;
+        this.price = price;
+        this.classes = classes.join(' ');
+        this.parent = document.querySelector(parentSelector);
+    }
+
+    render(){
+        this.el = document.createElement('div');
+        if (this.classes !='') {this.el.classList.add(this.classes);}
+        this.el.innerHTML=`
+            <div class="menu__item">
+                <img src=${this.src} alt=${this.alt}>
+                <h3 class="menu__item-subtitle">${this.title}</h3>
+                <div class="menu__item-descr">${this.descr}</div>
+                <div class="menu__item-divider"></div>
+                <div class="menu__item-price">
+                    <div class="menu__item-cost">Цена:</div>
+                    <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+                </div>
+            </div>
+        `;
+        this.parent.append(this.el);
+    }
+
+}
+
+new Menu (
+        'Меню "Фитнес"',
+        'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
+        '"img/tabs/vegy.jpg"',
+        '"vegy"',
+        229,
+        '.menu .container',
+        'menu__item'
+        ).render();
+new Menu (
+    'Меню “Премиум”',
+    'В меню “Премиум” мы используем не только красивый дизайн упаковки, но и качественное исполнение блюд. Красная рыба, морепродукты, фрукты - ресторанное меню без похода в ресторан!',
+    "img/tabs/elite.jpg",
+    '"elite"',
+    550,
+    '.menu .container',
+    'menu__item'
+    ).render();
+new Menu (
+    'Меню "Постное"',
+    'Меню “Постное” - это тщательный подбор ингредиентов: полное отсутствие продуктов животного происхождения, молоко из миндаля, овса, кокоса или гречки, правильное количество белков за счет тофу и импортных вегетарианских стейков.',
+    '"img/tabs/post.jpg"',
+    '"post"',
+    430,
+    '.menu .container',
+    'menu__item'
+    ).render();
 });
 
